2eb0ec7a0bde4cd0e471048461bd475e982e94c5
mohamadsabori <m.sabori.86@gmail.com>
2024-07-10 12:44:52 +0200

    Initial

---
+++ src/main/java/com/nl/techvallunar/App.java
@@ -0,0 +1,13 @@
+package com.nl.techvallunar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
